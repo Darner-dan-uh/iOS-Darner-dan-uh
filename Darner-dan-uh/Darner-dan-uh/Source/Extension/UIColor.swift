f8f56e80273e8e329e2586c1d30f5b2df09fe82e
이현욱 <hjhsha827@gmail.com>
2020-10-28 12:08:13 +0900

    [Add] UIColor Extension

--- Darner-dan-uh/Darner-dan-uh/Source/Extension/UIColor.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/Extension/UIColor.swift
@@ -13,6 +13,10 @@ extension UIColor {
         return rgb(red: 253, green: 125, blue: 128, alpha: 1)
     }
     
+    static var customGray: UIColor {
+        return rgb(red: 242, green: 242, blue: 242, alpha: 0.72)
+    }
+    
     static func rgb(red: Int, green: Int, blue: Int, alpha: CGFloat) -> UIColor {
         return .init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: alpha)
     }
