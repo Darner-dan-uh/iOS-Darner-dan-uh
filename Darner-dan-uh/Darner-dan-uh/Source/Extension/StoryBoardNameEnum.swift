f90c9780c015a3596c0bab4ce7d121b3fe972977
이현욱 <hjhsha827@gmail.com>
2020-11-02 18:23:31 +0900

    [Add] StroyBoardNameEnum

---
+++ Darner-dan-uh/Darner-dan-uh/Source/Extension/StoryBoardNameEnum.swift
@@ -0,0 +1,17 @@
+//
+//  StoryBoardNameEnum.swift
+//  Darner-dan-uh
+//
+//  Created by 이현욱 on 2020/11/02.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+enum ViewControllerName: String {
+    case memoriazeMainVC = "MemorizationViewController"
+    case selectwordNumVC = "SelectWordNumViewController"
+    case testTableVC = "TestTableViewController"
+    case testVC = "TestViewController"
+    case testResultVC = "TestResultViewController"
+}
