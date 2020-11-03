56c57643cec4d82ebe5568de123d1f6cf877f667
이현욱 <hjhsha827@gmail.com>
2020-11-04 00:55:32 +0900

    [Modify] UIViewController
    Extension

--- Darner-dan-uh/Darner-dan-uh/Source/Extension/UIViewController.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/Extension/UIViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 extension UIViewController {
-    func presentAlert(title: String, message: String, style: UIAlertController.Style, firstAction: UIAlertAction, secAction: UIAlertAction? = nil) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+    func presentAlert(message: String, style: UIAlertController.Style? = .alert, title: String, actionStyle: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?, secTitle: String? = nil, secActionStyle: UIAlertAction.Style? = nil, secHandler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: "     ", message: message, preferredStyle: .alert)
+        let titleFont = [NSAttributedString.Key.font: UIFont(name: "BinggraeMelona", size: 17)]
+        let titleAttrString = NSMutableAttributedString(string: message, attributes: titleFont)
+        alert.setValue(titleAttrString, forKey:"attributedMessage")
+        
+        alert.addAction(UIAlertAction(title: title, style: actionStyle, handler: handler))
+        if let existSecTitle = secTitle { alert.addAction(UIAlertAction(title: existSecTitle, style: secActionStyle!, handler: secHandler)) }
         alert.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = .customGray
-        alert.view.tintColor = .customPink
-        alert.addAction(firstAction)
-        if let sec = secAction {
-            alert.addAction(sec)
-        }
+        alert.view.tintColor = .black
         present(alert, animated: true, completion: nil)
     }
     
+    func makeVC<T>(storyBoardName: String? = nil, identifier: ViewControllerName) -> T where T: UIViewController {
+        //        let storyBoard = UIStoryboard(name: storyBoardName!, bundle: nil)
+        let vc = storyboard?.instantiateViewController(identifier: identifier.rawValue) as! T
+        return vc
+    }
 }
