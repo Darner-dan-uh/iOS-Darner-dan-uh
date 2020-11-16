9526fc1239b664775a31b1f219de4dcc7cf39c18
JI-doo <awt0311@gmail.com>
2020-11-16 18:56:36 +0900

    [Add] MypageModel

---
+++ Darner-dan-uh/Darner-dan-uh/Mypage/MypageModel.swift
@@ -0,0 +1,15 @@
+//
+//  MypageModel.swift
+//  Darner-dan-uh
+//
+//  Created by 문지수 on 2020/11/05.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import Foundation
+
+struct MypageModel: Codable {
+    let nickName: String
+    let password: String
+    let profileImage: String
+}
