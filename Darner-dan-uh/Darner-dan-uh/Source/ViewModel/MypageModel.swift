a867e244d69617199b3b3c9c0c1bc02b230102eb
이현욱 <hjhsha827@gmail.com>
2020-11-16 20:17:05 +0900

    [Modify]

---
+++ Darner-dan-uh/Darner-dan-uh/Source/ViewModel/MypageModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageModel.swift
+//  Darner-dan-uh
+//
+//  Created by 문지수 on 2020/10/15.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import Foundation
+
+struct MypageModel: Codable {
+    
+    let profileImage: String
+    let nickName: String
+    let ModifyProgile: String
+    let selectCharacter: String
+    
+}
