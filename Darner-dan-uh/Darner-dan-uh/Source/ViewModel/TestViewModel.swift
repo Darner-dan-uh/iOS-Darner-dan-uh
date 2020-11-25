6948d1d50134b98e8efa9ef5e3abbedfaede9774
이현욱 <hjhsha827@gmail.com>
2020-11-25 23:48:54 +0900

    [Modify] TestViewModel

--- Darner-dan-uh/Darner-dan-uh/Source/ViewModel/TestViewModel.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/ViewModel/TestViewModel.swift
@@ -12,16 +12,18 @@ import RxCocoa
 import RxSwift
 
 class TestViewModel: ViewModelType {
+    var word: String!
     struct Input {
         let meanSubject: Observable<String>
         let nextWordTap: Single<Void>
     }
     
     struct Output {
-        let resultWord: Observable<[WordCell]>
+        let resultWord: Observable<Bool>
     }
     
     func transform(_ input: Input) -> Output {
-        return Output(input.meanSubject.)
+        return Output(resultWord: input.meanSubject.map { $0.isEmpty }.asObservable())
+            
     }
 }
