1e8bb6e8bf2e501359a9603d37cbb5029a26d0b8
이현욱 <hjhsha827@gmail.com>
2020-11-25 23:47:40 +0900

    [Modify] LoginViewController

--- Darner-dan-uh/Darner-dan-uh/Source/Modules/Login/LoginViewController.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/Modules/Login/LoginViewController.swift
@@ -12,9 +12,8 @@ import RxSwift
 import RxCocoa
 
 final class LoginViewController: UIViewController {
-    
     var autoLoginFlag: Bool! = false
-    let viewModel = LoginViewModel()
+    let viewModel: LoginViewModel = LoginViewModel()
     let disposeBag = DisposeBag()
     
     @IBOutlet weak var idTextField: UITextField!
@@ -27,46 +26,65 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         bindAction()
-        bindUI()
+        bind()
     }
 }
 
+
+
 extension LoginViewController {
     private func bindAction() {
+        loginBtn.rx.tap
+            .debounce(.seconds(2), scheduler: MainScheduler.instance)
+            .withLatestFrom(Observable.combineLatest(self.idTextField.rx.text.orEmpty, self.pwTextField.rx.text.orEmpty))
+            .map { DarnerAPI.login(userId: $0.0, password: $0.1) }
+            .flatMap { (request:DarnerAPI) -> Observable<LoginResultModel> in
+                return DarnerAPIClient.shared.networkingResult(from: request)
+            }
+            .subscribe { (model:LoginResultModel) in
+                if model.message == "UnAuthorized" {
+                    self.loginBtn.shake()
+                    return
+                }
+                //                    b.message 키체인에 저장
+                let vc = self.makeVC(identifier: ViewControllerName.tabbarVC)
+                vc.modalPresentationStyle = .fullScreen
+                DispatchQueue.main.async {
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            } onError: { (_:Error) in
+                self.loginBtn.shake()
+            }.disposed(by: self.disposeBag)
+        
+        Observable.merge(
+            autoLoginBtn.rx.tap.map {},
+            autoLoginLblBtn.rx.tap.map {}
+        ).map { self.autoLoginBtn.isSelected.toggle() }
+        .map { if self.autoLoginBtn.isSelected == true {
+            self.autoLoginBtn.setImage(UIImage(named: "Icon ionic-ios-checkmark-circle"), for: .selected)
+        } else {
+            self.autoLoginBtn.setImage(UIImage(named: "Icon material-radio-button-unchecked"), for: .normal)
+        }}
+        .subscribe()
+        .disposed(by: disposeBag)
+        
         popVCBtn.rx.tap
             .subscribe { _ in
                 self.navigationController?.popViewController(animated: true)
             }.disposed(by: disposeBag)
-        
-        loginBtn.rx.tap
-            .map { /* 서버 통신 */ }
-            .map { /* 자동 로그인에 대한 처리 */}
-            .subscribe { _ in
-                let vc = self.makeVC(identifier: ViewControllerName.tabbarVC)
-                vc.modalPresentationStyle = .fullScreen
-                self.present(vc, animated: true, completion: nil)
-            }.disposed(by: disposeBag)
-        
-        Observable.merge(autoLoginBtn.rx.tap.map {}, autoLoginLblBtn.rx.tap.map {})
-            .map { self.autoLoginBtn.isSelected.toggle() }
-            .map { if self.autoLoginBtn.isSelected == true {
-                self.autoLoginBtn.setImage(UIImage(named: "Icon ionic-ios-checkmark-circle"), for: .selected)
-            } else {
-                self.autoLoginBtn.setImage(UIImage(named: "Icon material-radio-button-unchecked"), for: .normal)
-            }}
-            .subscribe { _ in
-                // keychain에 유저 정보 저장
-            }.disposed(by: disposeBag)
     }
     
-    private func bindUI() {
-        idTextField.rx.text.map { $0 ?? "" }.bind(to: viewModel.idTextFieldSubject).disposed(by: disposeBag)
-        pwTextField.rx.text.map { $0 ?? "" }.bind(to: viewModel.pwTextFieldSubject).disposed(by: disposeBag)
-        viewModel.isValid().subscribe(onNext :{ value in
-            self.loginBtn.layer.borderColor = value ? UIColor.customPink.cgColor : UIColor.black.cgColor
-            self.loginBtn.backgroundColor = value ? .customPink : .white
-            value ? self.loginBtn.setTextColor(color: .white) : self.loginBtn.setTextColor(color: .black)
-            self.loginBtn.isUserInteractionEnabled = value
+    private func bind() {
+        let input = LoginViewModel.Input.init(idTextFieldSubject: idTextField.rx.text.orEmpty.asObservable(), pwTextFieldSubject: pwTextField.rx.text.orEmpty.asObservable())
+        
+        let output = viewModel.transform(input)
+        
+        output.result.drive (onNext:{ b in
+            self.loginBtn.layer.borderColor = b ? UIColor.customPink.cgColor : UIColor.black.cgColor
+            self.loginBtn.backgroundColor = b ? .customPink : .white
+            b ? self.loginBtn.setTextColor(color: .white) : self.loginBtn.setTextColor(color: .black)
+            self.loginBtn.isUserInteractionEnabled = b
         }).disposed(by: disposeBag)
     }
 }
+
