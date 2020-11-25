fb061b9f02f92dbbfb606600d06fdcf65061a1e4
이현욱 <hjhsha827@gmail.com>
2020-11-25 23:51:21 +0900

    [Modify] RegisterViewController

--- Darner-dan-uh/Darner-dan-uh/Source/Modules/Register/RegisterViewController.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/Modules/Register/RegisterViewController.swift
@@ -12,18 +12,19 @@ import RxSwift
 import RxCocoa
 
 final class RegisterViewController: UIViewController {
+    static var email: String!
     
     let disposeBag = DisposeBag()
     let viewModel = RegisterViewModel()
     
     @IBOutlet weak var sameIdNoticeLbl: UILabel!
     @IBOutlet weak var wrongPwLbl: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var registerBtn: UIButton!
     @IBOutlet weak var popVCBtn: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,51 +35,20 @@ final class RegisterViewController: UIViewController {
 
 extension RegisterViewController {
     private func bindUI() {
-        nameTextField.rx.text
-            .map { $0 ?? "" }
-            .bind(to: viewModel.nameTextFieldSubject)
-            .disposed(by: disposeBag)
         
-        idTextField.rx.text
-            .map { $0 ?? "" }
-            .bind(to: viewModel.idTextFieldSubject)
-            .disposed(by: disposeBag)
+        let input = RegisterViewModel.Input.init(nameTextFieldSubject: nameTextField.rx.text.orEmpty.asObservable(),
+                                                 idTextFieldSubject: idTextField.rx.text.orEmpty.asObservable(),
+                                                 pwTextFieldSubject: pwTextField.rx.text.orEmpty.asObservable(),
+                                                 emailTextFieldSubject: emailTextField.rx.text.orEmpty.asObservable())
         
-        pwTextField.rx.text
-            .map { $0 ?? "" }
-            .bind(to: viewModel.pwTextFieldSubject)
-            .disposed(by: disposeBag)
+        let output = viewModel.transform(input)
         
-        nameTextField.rx.text.map { $0 ?? "" }.bind(to: viewModel.nameTextFieldSubject).disposed(by: disposeBag)
-        
-        idTextField.rx.text.map { $0 ?? "" }.bind(to: viewModel.idTextFieldSubject).disposed(by: disposeBag)
-        
-        pwTextField.rx.text.map { $0 ?? "" }.bind(to: viewModel.pwTextFieldSubject).disposed(by: disposeBag)
-        
-        viewModel.isValid().subscribe(onNext: { value in
-            self.registerBtn.layer.borderColor = value ? UIColor.customPink.cgColor : UIColor.black.cgColor
-            self.registerBtn.backgroundColor = value ? .customPink : .white
-            value ? self.registerBtn.setTextColor(color: .white) : self.registerBtn.setTextColor(color: .black)
-            self.registerBtn.isUserInteractionEnabled = value
+        output.signal.drive(onNext: { b in
+            self.registerBtn.layer.borderColor = b ? UIColor.customPink.cgColor : UIColor.black.cgColor
+            self.registerBtn.backgroundColor = b ? .customPink : .white
+            b ? self.registerBtn.setTextColor(color: .white) : self.registerBtn.setTextColor(color: .black)
+            self.registerBtn.isUserInteractionEnabled = b
         }).disposed(by: disposeBag)
-        
-        idTextField.rx.text
-            .subscribe( { text in
-                // text를 넣은 후 네트워킹해서 그 결과값을 sameIdNoticeLbl에 사용
-            }).disposed(by: disposeBag)
-        
-        pwTextField.rx.text
-            .map { self.viewModel.checkPw(PassWord: $0!)}
-            .subscribe { bool in
-                self.wrongPwLbl.isHidden = bool
-            }.disposed(by: disposeBag)
-        
-        idTextField.rx.text.subscribe( { text in
-            // text를 넣은 후 네트워킹해서 그 결과값을 sameIdNoticeLbl에 사용
-        }).disposed(by: disposeBag)
-        pwTextField.rx.text.map { self.viewModel.checkPw(PassWord: $0!)}.subscribe { bool in
-            self.wrongPwLbl.isHidden = bool
-        }.disposed(by: disposeBag)
     }
     
     private func bindAction() {
@@ -87,11 +57,31 @@ extension RegisterViewController {
                 self.navigationController?.popViewController(animated: true)
             }.disposed(by: disposeBag)
         
-        registerBtn.rx.tap.map {
-            // 이름, 아이디, 비밀번호를 서버에 보냄
-        }.subscribe { _ in
-            let vc = self.makeVC(identifier: ViewControllerName.doubleCheckVC)
-            self.navigationController?.pushViewController(vc, animated: true)
-        }.disposed(by: disposeBag)
+        registerBtn.rx.tap
+            .debounce(.seconds(2), scheduler: MainScheduler.instance)
+            .withLatestFrom(Observable.combineLatest(self.idTextField.rx.text.orEmpty,
+                                                     self.emailTextField.rx.text.orEmpty,
+                                                     self.pwTextField.rx.text.orEmpty,
+                                                     self.nameTextField.rx.text.orEmpty))
+            .map {
+                RegisterViewController.email = $0.1;
+                return DarnerAPI.signup(userId: $0.0, name: $0.3, email: $0.1, password: $0.2)
+            }
+            .flatMap { (request:DarnerAPI) -> Observable<MessageModel> in
+                return DarnerAPIClient.shared.networkingResult(from: request)
+            }
+            .subscribe { model in
+                if model.message == "success" {
+                    let vc: CheckCertificationNumberViewController = self.makeVC(identifier: .checkCertificationVC)
+                    DispatchQueue.main.async {
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                } else {
+                    self.registerBtn.shake()
+                }
+            } onError: { _ in
+                self.registerBtn.shake()
+            }
+            .disposed(by: disposeBag)
     }
 }
