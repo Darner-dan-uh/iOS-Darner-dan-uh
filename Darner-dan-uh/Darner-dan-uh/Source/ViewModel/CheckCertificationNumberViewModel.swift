0bd2c3073d08f619411f11a016a97443a1674595
이현욱 <hjhsha827@gmail.com>
2020-11-25 23:48:32 +0900

    [Modify] CheckCertificationNumberViewModel

--- Darner-dan-uh/Darner-dan-uh/Source/ViewModel/CheckCertificationNumberViewModel.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/ViewModel/CheckCertificationNumberViewModel.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 이현욱. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+class CheckCertificationNumberViewModel: ViewModelType {
+    struct Input {
+        let codeSubject: Observable<String>
+    }
+    
+    struct Output {
+        let result: Driver<Bool>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        let bool = input.codeSubject
+            .map { $0.isEmpty }
+            .asDriver(onErrorJustReturn: false)
+        return Output(result: bool)
+    }
+}
