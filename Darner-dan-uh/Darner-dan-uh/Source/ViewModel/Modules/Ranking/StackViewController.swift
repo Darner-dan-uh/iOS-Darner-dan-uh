8723ed0ad6a12d5ec725245685fbde0e739abac6
JI-doo <awt0311@gmail.com>
2020-12-03 13:34:30 +0900

    [Add] StackViewController

---
+++ Darner-dan-uh/Darner-dan-uh/Source/ViewModel/Modules/Ranking/StackViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StackViewController.swift
+//  Darner-dan-uh
+//
+//  Created by 문지수 on 2020/12/03.
+//  Copyright © 2020 이현욱. All rights reserved.
+//
+
+import UIKit
+
+class StackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
