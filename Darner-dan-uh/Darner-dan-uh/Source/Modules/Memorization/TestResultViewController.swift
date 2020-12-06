809fd247b54eab2ace33e098f31513e0837626fa
이현욱 <hjhsha827@gmail.com>
2020-12-06 23:18:03 +0900

    [Modify] Code

--- Darner-dan-uh/Darner-dan-uh/Source/Modules/Memorization/TestResultViewController.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/Modules/Memorization/TestResultViewController.swift
@@ -12,7 +12,9 @@ import RxSwift
 import RxCocoa
 
 final class TestResultViewController: UIViewController {
-    var arr: [TestRusultModel]?
+    var arr: Observable<[TestRusultModel]>! = nil
+    let disposeBag = DisposeBag()
+    var correctCnt = 0
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var finishWordTestBtn: UIButton!
@@ -23,33 +25,47 @@ final class TestResultViewController: UIViewController {
         bindUI()
         // Do any additional setup after loading the view.
     }
-    
-    
 }
 
 extension TestResultViewController {
     private func bindUI() {
-        print(arr)
-        Observable.from(optional: arr)
-            .bind(to: tableView.rx.items(cellIdentifier: WordResultCell.cellName, cellType: WordResultCell.self)) { _, result, cell in
-                cell.wordMeanLbl.text = result.word
-                if result.correct {
-                    cell.isCorrectLbl.text = "정답"
-                    cell.isCorrectLbl.textColor = .customPink
-                } else {
-                    cell.isCorrectLbl.text = "오답"
-                    cell.isCorrectLbl.textColor = .customGray
-                }
-            }.dispose()
+        arr.bind(to: tableView.rx.items(cellIdentifier: WordResultCell.cellName, cellType: WordResultCell.self)) { _, result, cell in
+            cell.wordMeanLbl.text = result.word
+            if result.correct {
+                cell.isCorrectLbl.text = "정답"
+                cell.isCorrectLbl.textColor = .customBlack
+                self.correctCnt += 1
+            } else {
+                cell.isCorrectLbl.text = "오답"
+                cell.isCorrectLbl.textColor = .customPink
+            }
+            print(self.correctCnt)
+        }.disposed(by: disposeBag)
+        
+        let id = UserDefaults.standard.value(forKey: "id") as! String
+        let os: Observable<MessageModel> = DarnerAPIClient.shared.networkingResult(from: .savedata(id: id, count: self.correctCnt))
+            .retry()
+            
+        os.subscribe(onNext: { model in
+            if model.message == "success" {
+                print("success")
+            } else {
+                print("fail")
+            }
+        }).disposed(by: disposeBag)
     }
     
     private func bindAction() {
         finishWordTestBtn.rx.tap
-            .map { var controller = self.navigationController?.viewControllers
-                controller?.removeAll()
-                self.navigationController?.viewControllers = controller!
+            .map { _ in
+                guard var controllers = self.navigationController?.viewControllers else { return }
+                let count = controllers.count
+                if count > 2 {
+                    controllers.removeSubrange(1...count-2)
+                }
+                self.navigationController?.viewControllers = controllers
                 self.navigationController?.popViewController(animated: true)
             }.subscribe()
-            .dispose()
+            .disposed(by: disposeBag)
     }
 }
