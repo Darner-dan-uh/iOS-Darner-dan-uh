5830e7b7ea4d772f30f62e071d18eda9155e069b
이현욱 <hjhsha827@gmail.com>
2020-12-06 23:17:31 +0900

    [Modify] Code

--- Darner-dan-uh/Darner-dan-uh/Source/Modules/MyPage/MypageViewController.swift
+++ Darner-dan-uh/Darner-dan-uh/Source/Modules/MyPage/MypageViewController.swift
@@ -32,27 +32,24 @@ class MypageViewController: UIViewController {
         
         bindAction()
         
-        let alret = UIAlertController(title: "로그아웃 하시겠습니까?", message: "", preferredStyle: UIAlertController.Style.alert)
-
-        alret.addAction(UIAlertAction(title: "네", style: .default, handler: nil))
-        alret.addAction(UIAlertAction(title: "아니요", style: .cancel, handler: nil))
-        
-        present(alret, animated: true, completion: nil)
-
-        
     }
     
     func bindAction() {
+        logOutBtn.rx.tap
+            .map { self.presentAlert(message: "로그아웃 하시겠습니까?", style: .alert, title: "네", actionStyle: .default, handler: nil, secTitle: "아니요", secActionStyle: .cancel, secHandler: nil)}
+            .subscribe()
+            .disposed(by: disposeBag)
+        
         modifyBtn.rx.tap
             .map{ let controller = self.navigationController?.viewControllers
                 self.navigationController?.viewControllers = controller!
                 self.navigationController?.popViewController(animated: true)
         }
-    }
-    
-    @IBAction func nextVC(_ sender: UIButton) {
-        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "modify") else { return }
-        self.present(nextVC, animated: true)
-        self.dismiss(animated: true)
+        
+        characterBtn.rx.tap
+            .subscribe(onNext: { _ in
+                let vc = self.makeVC(storyBoardName: .myPage, identifier: .myCharacterVC)
+                self.present(vc, animated: true, completion: nil)
+            }).disposed(by: disposeBag)
     }
 }
